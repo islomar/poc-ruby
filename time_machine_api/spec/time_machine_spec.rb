dca4b9e75df95ff4888853ff410244aff09f63ce
Isidro López <islomar@gmail.com>
2018-05-02 16:22:56 +0200

    [time-machine-api] [test] Change test order and include app() inside root describe

--- time_machine_api/spec/time_machine_spec.rb
+++ time_machine_api/spec/time_machine_spec.rb
@@ -7,28 +7,17 @@ require 'logger'
 
 include Rack::Test::Methods
 
-def app
-  factory = Factory.new
-  time_machine_service = factory.time_machine_service
-  TimeMachineAPI.new(time_machine_service)
-end
-
 ANY_VALID_ISO8601_TIME = "2018-04-23T16:37:01+02:00"
 ANY_INVALID_ISO8601_TIME = "invalid-iso8601-time"
 INVALID_ISO8601_TIME_ERROR_MESSAGE = "The date passed must have a ISO8601 format"
 
 
 describe "TimeMachineAPI" do
-  describe "GET /time" do
-    it "returns the time in iso8601 format" do
-      allow(DateTime).to receive(:now).and_return(DateTime.parse(ANY_VALID_ISO8601_TIME))
-      get '/time', { 'ACCEPT': 'application/json' }
 
-      expect(last_response.status).to eq HTTP::Status::OK
-      expect(last_response.body).not_to be_empty
-      assert_content_is_json(last_response)
-      assert_response_contains_expected_iso8601_time(last_response, ANY_VALID_ISO8601_TIME)
-    end
+  def app
+    factory = Factory.new
+    time_machine_service = factory.time_machine_service
+    TimeMachineAPI.new(time_machine_service)
   end
 
   describe "POST /time" do
@@ -52,11 +41,23 @@ describe "TimeMachineAPI" do
       end
     end
 
-    it "returns 400 (Bad Request) if the time to be frozen does not have ISO8601 format" do
-      post_json('/time/' + ANY_INVALID_ISO8601_TIME)
+      it "returns 400 (Bad Request) if the time to be frozen does not have ISO8601 format" do
+        post_json('/time/' + ANY_INVALID_ISO8601_TIME)
 
-      expect(last_response.status).to eq HTTP::Status::BAD_REQUEST
-      expect(last_response.body).to eq(INVALID_ISO8601_TIME_ERROR_MESSAGE)
+        expect(last_response.status).to eq HTTP::Status::BAD_REQUEST
+        expect(last_response.body).to eq(INVALID_ISO8601_TIME_ERROR_MESSAGE)
+      end
+
+    describe "GET /time" do
+      it "returns the time in iso8601 format" do
+        allow(DateTime).to receive(:now).and_return(DateTime.parse(ANY_VALID_ISO8601_TIME))
+        get '/time', { 'ACCEPT': 'application/json' }
+
+        expect(last_response.status).to eq HTTP::Status::OK
+        expect(last_response.body).not_to be_empty
+        assert_content_is_json(last_response)
+        assert_response_contains_expected_iso8601_time(last_response, ANY_VALID_ISO8601_TIME)
+      end
     end
   end
 
